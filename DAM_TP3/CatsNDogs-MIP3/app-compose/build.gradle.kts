8816b5976e923bd1e71ab27a85eb66dd30fab1e0
hsps1999 <hsps1999@gmail.com>
2026-05-03 17:40:28 +0100

    chore(app-compose): add Compose, Navigation and Coil dependencies

--- DAM_TP3/CatsNDogs-MIP3/app-compose/build.gradle.kts
+++ DAM_TP3/CatsNDogs-MIP3/app-compose/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -33,7 +34,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
     buildFeatures {
-        compose = false
+        compose = true
     }
     packaging {
         resources {
@@ -47,8 +48,24 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.activity)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.navigation.compose)
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+
+    implementation(libs.coil.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
