f69ff7bd3ade3d6cc27e5cfd17cd623260eeb514
hsps1999 <hsps1999@gmail.com>
2026-05-03 17:43:34 +0100

    feat(app-compose): add Material 3 theme

---
+++ DAM_TP3/CatsNDogs-MIP3/app-compose/src/main/java/dam_a46104/catsndogs/compose/ui/theme/Color.kt
@@ -0,0 +1,33 @@
+package dam_a46104.catsndogs.compose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryLight = Color(0xFF6750A4)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFFEADDFF)
+val OnPrimaryContainerLight = Color(0xFF21005D)
+
+val SecondaryLight = Color(0xFF625B71)
+val OnSecondaryLight = Color(0xFFFFFFFF)
+val SecondaryContainerLight = Color(0xFFE8DEF8)
+val OnSecondaryContainerLight = Color(0xFF1D192B)
+
+val BackgroundLight = Color(0xFFFFFBFE)
+val OnBackgroundLight = Color(0xFF1C1B1F)
+val SurfaceLight = Color(0xFFFFFBFE)
+val OnSurfaceLight = Color(0xFF1C1B1F)
+
+val PrimaryDark = Color(0xFFD0BCFF)
+val OnPrimaryDark = Color(0xFF381E72)
+val PrimaryContainerDark = Color(0xFF4F378B)
+val OnPrimaryContainerDark = Color(0xFFEADDFF)
+
+val SecondaryDark = Color(0xFFCCC2DC)
+val OnSecondaryDark = Color(0xFF332D41)
+val SecondaryContainerDark = Color(0xFF4A4458)
+val OnSecondaryContainerDark = Color(0xFFE8DEF8)
+
+val BackgroundDark = Color(0xFF1C1B1F)
+val OnBackgroundDark = Color(0xFFE6E1E5)
+val SurfaceDark = Color(0xFF1C1B1F)
+val OnSurfaceDark = Color(0xFFE6E1E5)
